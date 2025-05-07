734712292b1d7c16ef1a6dc4ea71ef39b1dd4c6a
ajithdev <ajithofficial2028@gmail.com>
2025-05-07 12:36:34 +0530

    Initial commit

---
+++ src/test/java/com/storesync/app/StoreSyncWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storesync.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreSyncWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
